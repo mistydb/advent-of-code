2b798f79c772e888559787f3e24730d522cfe327
mistydb <mistydbaird@gmail.com>
2022-12-07 22:21:44 -0700

    Day 05 solution

--- 2022/day05.js
+++ 2022/day05.js
@@ -42,33 +42,34 @@ let crates = [
     });
     console.log(mv);
   
-    function moveCrate(crates) {
+    function moveCrate9001(crates) {
       for (let m = 0; m < mv.length; m++) {
-        let moves = Number(mv[m].amount);
-        let indexCrateTo = Number(mv[m].from) - 1;
-        let indexCrateFrom = Number(mv[m].to) - 1;
-        for (let i = 0; i < moves; i++) {
-          crates[indexCrateTo].push(crates[indexCrateFrom].pop());
-        }
+        let numberToSplice = Number(mv[m].amount);
+        let to = Number(mv[m].to) - 1;
+        let from = Number(mv[m].from) - 1;
+  
+        crates[to].push(
+          ...crates[from].splice(crates[from].length - numberToSplice)
+        );
       }
     }
   
-    moveCrate(crates);
+    moveCrate9001(crates);
     getAnswer();
-  };
   
-  function getAnswer() {
-    console.log(
-      crates[0][crates[0].length - 1],
-      crates[1][crates[1].length - 1],
-      crates[2][crates[2].length - 1],
-      crates[3][crates[3].length - 1],
-      crates[4][crates[4].length - 1],
-      crates[5][crates[5].length - 1],
-      crates[6][crates[6].length - 1],
-      crates[7][crates[7].length - 1],
-      crates[8][crates[8].length - 1]
-    );
-  }
+    function getAnswer() {
+      console.log(
+        crates[0][crates[0].length - 1],
+        crates[1][crates[1].length - 1],
+        crates[2][crates[2].length - 1],
+        crates[3][crates[3].length - 1],
+        crates[4][crates[4].length - 1],
+        crates[5][crates[5].length - 1],
+        crates[6][crates[6].length - 1],
+        crates[7][crates[7].length - 1],
+        crates[8][crates[8].length - 1]
+      );
+    }
+  };
   getInput();
   
