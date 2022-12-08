45be1f7674ddc3e1e7adf6e9304744a56a6f2011
mistydb <mistydbaird@gmail.com>
2022-12-07 22:25:06 -0700

    Day 06 Solution

---
+++ 2022/day06.js
@@ -0,0 +1,107 @@
+let data =
+  "qnjjqgjqgglqqwrrvvcwwtjjzpjjfwwtwzwmzmrmttrvrpppbtbllhrrwtwzwjwzznfzfvvvngnrrhzrhhsmsrrsrrtqrrpzpnpfphfhllwlclfcfrrqffhvhsvswvswwngnzzcmmnrnzrnnwhwgwvggpcggnrntrrgmgdgmmgzmzssprrmddjndnsnrsrddnmdmqddrzznwznndfnndjjmgjmjmmnpmnmrmzmfzmmwhwzwrwqqqhshhncnvvhfvhfhzzvttrnrdnnwddptphpthhbgbgmbbvppnhphmmncnwcwqwppgddsqqjqppcrcncvnccjsjzztczttjzzwwhshnsnttrctchthnnswnnfzfhhsfhhdccgwcggbrblrlwlrlnntsscffpnnwffqgqdqhqhphhvtvmvhmmnssqbqfqfwwbfwbfwwzwgwgpgwwchcshslhssqsppqdqhhnssrzzvbbhjjqdjdgjgttvzzmvzvvnmvvfbbncbcqcpqpbbcjcncjnccjfjpjdjvdddbzzfrfwfpwpfwppvjvfjjhfhvvqttfhffscsvvbqbzqqjssbmsbshsrshsphhwhvwhwcwwnjjlzzfgfrgrwggfddpqpqqcmmtwtcwttgjjnfnnjvvmfmccvhchjcjmccmmwgghccnffjccqczqqbdbrdrsdsffmzzndnncbnbgbsshbhvbvsbvsbbvjbvbdvvqpqnpngpnggbbndbbdzdhzdhhjcjjmbbqgqbbfrrvzvcvffhggcbcjbbqtbbszbbvvcnvnccldddlrrtqrttpjpnjpnpznnqmnnrrmmsdmdbbffcwwllnffssspvvrjjwhwmmqhmmhjjtnjtjgttnrtnnsgsbssffstssjvjwwwwnjngjnnnlndndqdqqwdwcdcsccsgssgrrqwqqqhgqgfgffmnffzjjssqsfqqgddgcgtgrrvgrrdhdcdgdbdsswgwwpwggdfggnjnnvffzhhcqclqclcdlldbldlffdrrzttnhthdthhrthhgcgllzqlzzfhhmshssdvvtnnpngpprqrrrbnbssnhnnqdndsnshhzjjrnjnmjjwfffpbpnbbwddpwptwwpbpffvggmjmgghrrnqqhnhsnhsnnnlddmjddrdrhrchhgmgfmfdfcfpprsrzzzmhzhmhrmmrwmmpdmpmcclmlvvlffhlhhczccsvvptvpvwvdvqqdvqdddvnvsnswwspwpqqpddpvvsqsjjwnwrrjbjtttdhhhwbhbzzqrqfrrzlrzrqrbqrqzzfczfzrrqffnrnhrrcrtrvrffvflfsllcjcvvmqmlqqwjwgglhlvldltlqqdcddtssltsllsnndvdnddfqdffprrbssgccqmqrrnsrnnlzlhhqrhqhcqcjqcqpcqqmvqqgbgpbggnfggdpgpvpgvvsrrhqhshmsmwsmwssvnvtnttphpdpdspsvslshhlplmplmmljjvnnsrslrrvttqqprrptrptpnnmmflltntrrnjjcljcllwcllddbgbjbrrtwrtrftfctftsslsggggzqqhhrvvdqqwqbqtthdhrrpwpqpmmqcmqmllsmllhvfwbfvgzqgbhlfhqrqtzfpplgjtgngzrdfltnqlwsbhmwdfvrdjlgwftjvmdsgdgnswhrtmzgfqfsfnczjnmgqcfzvwlbgzsrpcbwwtrmbqtqhdmhmscqgjgpqdqcrvdvwplpdzsjqbvgpqcvfspqrcsjlhrqpjmdszjpqhmdwtddwqhbwsrlcjpzwsjjvbzcllqfwbhfvjqbqfbsrbgvgchdmgqjnvdrzlmrnlpzrljgjvtrdtqnzbnhpgpgjvwttmnfbpvrtmpstbmtwdwfzvznrwmspftgvrmdfwqltzzmlgrvvwjgdblnnbjzjfqpldsqbhrstnhrjqfcmzcrtqcpqmmfqzndgjwtcgnwrgdznzdgzbvmrlvrjvjgmfcrmrjbpjwqvhprbphnqsbpcpnflpgnpgggqpgrwghfpfvdljjqnvqgbvcpjbjlqghjppfhzfzczmcwnhrjzzrwlfqhvdwqrcbvprclnmqmqwdrhgtswwqhqtcjhndrmcrzdjnvwsstzplhcdzwzqbjjthsmjrmpfbqljlmnvmfddqflnhrfhchzgnlbcpvppnspsdtnqmfhrztznhltmqlrwzgczqmjggvnphwpvrwcnqhsdfglblvvdlqfnghtzgngtjnrzsjgwcglsrczlfqpmmzsrqwcclslzjpvpfgcwzwsfmwwswdsvnvtmwzmhwzvnnnldnjljbwlglpjvrggbmvhctsggtjgrmjrglnhtjzwhrvtmfmphgmpdmvlhzrhrtjcwswlnnjphsschrdvstmflzqsbqmqggjjdsdtjhcvjlzclbpvnbjgngnzvtdgszdtrwpnjmgwpcjpbsvrsmdjdjdbhqqvplbmfhmbgmtggvtltqjphlggscdbzncqhqqlmflmjbpnhbnqjcbfmggnftjjbzbrqwcgvfhsddrqgrvnztzzqzwjrjprmqctlttmgltdgrcvnbrbzrsbbqnsrslrswvmrqlctvsdfmcrdddcdwvfmwwcbqsdhmmgplzhnsfvhtctmvrzntvptpnbhhvjlphmtcctlhdvtvvqbmbczjqfvnqwzbgjzlgnqhvbjjnvrgfprvllltqdwvvmchrdqfczmmbmcjwtwgqbfjzgpcfzhhnwcghqltrlhlntjcrthpsbmcdzzqljdrdhfctwrzlvhdjvgmpscssmhdggrnhfbtfpqtsbqnqjfjwlbdfcpzfjtspnrjzshjcrrzwclddqqbplwghrcjhfqgblfrlrdhmdqmrzdnqthvvzlcjmqzqtgbcwcprpnjtbhsqmzlprhmssltfvvgqzzpnjtsmplhfpsnznjwrrgvmbbvjzzwctmwcwcwjwvmlpcmlmrfqbdjpclsqjnsnndzqfcghqsmhqcjwjjbbwmsttwnththhhgrlhrtrcjppvmjlqtqhpbhpsdhzcqpmqqbvwrlvjrnmlfvtntwghtphwzqmbmmpgvfrqsjwffdbcjjbwrrntrhnjwjfprzjcpnwvtwjcdhppspbdbwnzdbhvpqljbrfnpnbqdtqzdjlbvgbvvhcjrqwlqmhnhjfppllrsnsvnmttvrsdrqjfrdvhplwbhhnvglvgcvtzblfswzrrbwpjnslthjmgsdgwqrfjfplmbgsdcltzmdhrbpqggnjrcddltvdbrlhtzwfghbpnlmghwqdtpctmqbggfzflfzjgcbvfqtztsmgdvdlrrdrtmmjbtdtjvvjvlwwlwnzhlfqtrmdgvlflmbffnzlvtccnmcddrqfhgzlhngrfmsqdrnshshfhhtnwmnqmjtzggdfmpgcsrhjzhhvlppfnqmjsvtcwwjdvdrgrldbdnrqtvjjnddgmfdtqrncdlfznfpbqldztzzgpvwhvnqrrzdwsrdvcnslmwcdztvqzgbshpqrdbnmlfltpbsfrnbbcwvwvjnrdspwrwzpljqwwvqhbhhqqjnlrgwqvvsjrscbfqvhbwtjvwzmwzvftlwmtbmglvqvlnwrzvwclvzjcvgfstjpwdbtbhmnqzfgpqmthpchrrqfpqfflwhwrhmdzvgbzlmfgzvhrldprlgrvjcvjhslghqddfvqzgplmtfqjzhpctvtgrzjhwrjzbsqqrcztrstbvdjbtszrbvvzvwzfclsnrftcqtqdghdrnhglzptzwtqtgnfsqhmvnqqwtnwszgtvpnndbzcwgwhpcdlsdpptvlcqtmfhfjzbvfsvnjvqrrjqbggnjqfrrrvqmzscvlltfwzdlzqstlrrmqqmbmbtgbqjsvbncblddgzvlstmrpmrlfcqpvwthgbhlvfcfcwvslcjnztfwgdgwjfrwrzbbszmtvzchvstcfrgqllfrdccrjmtspshgqbzcldddqgjnpdsctmjphcwqvjtvmqlvvpzdgjdlpwdrjbshhrgjtglwnzlzsqngspbzgbwpgmfcv";
+let inputArray = data.split("");
+
+console.log(inputArray);
+
+function checkUnique(input) {
+  for (let i = 13; i < input.length; i++) {
+    if (
+      !(input[i] === input[i - 1]) &&
+      !(input[i] === input[i - 2]) &&
+      !(input[i] === input[i - 3]) &&
+      !(input[i] === input[i - 4]) &&
+      !(input[i] === input[i - 5]) &&
+      !(input[i] === input[i - 6]) &&
+      !(input[i] === input[i - 7]) &&
+      !(input[i] === input[i - 8]) &&
+      !(input[i] === input[i - 9]) &&
+      !(input[i] === input[i - 10]) &&
+      !(input[i] === input[i - 11]) &&
+      !(input[i] === input[i - 12]) &&
+      !(input[i] === input[i - 13]) &&
+      !(input[i - 1] === input[i - 2]) &&
+      !(input[i - 1] === input[i - 3]) &&
+      !(input[i - 1] === input[i - 4]) &&
+      !(input[i - 1] === input[i - 5]) &&
+      !(input[i - 1] === input[i - 6]) &&
+      !(input[i - 1] === input[i - 7]) &&
+      !(input[i - 1] === input[i - 8]) &&
+      !(input[i - 1] === input[i - 9]) &&
+      !(input[i - 1] === input[i - 10]) &&
+      !(input[i - 1] === input[i - 11]) &&
+      !(input[i - 1] === input[i - 12]) &&
+      !(input[i - 1] === input[i - 13]) &&
+      !(input[i - 2] === input[i - 3]) &&
+      !(input[i - 2] === input[i - 4]) &&
+      !(input[i - 2] === input[i - 5]) &&
+      !(input[i - 2] === input[i - 6]) &&
+      !(input[i - 2] === input[i - 7]) &&
+      !(input[i - 2] === input[i - 8]) &&
+      !(input[i - 2] === input[i - 9]) &&
+      !(input[i - 2] === input[i - 10]) &&
+      !(input[i - 2] === input[i - 11]) &&
+      !(input[i - 2] === input[i - 12]) &&
+      !(input[i - 2] === input[i - 13]) &&
+      !(input[i - 3] === input[i - 4]) &&
+      !(input[i - 3] === input[i - 5]) &&
+      !(input[i - 3] === input[i - 6]) &&
+      !(input[i - 3] === input[i - 7]) &&
+      !(input[i - 3] === input[i - 8]) &&
+      !(input[i - 3] === input[i - 9]) &&
+      !(input[i - 3] === input[i - 10]) &&
+      !(input[i - 3] === input[i - 11]) &&
+      !(input[i - 3] === input[i - 12]) &&
+      !(input[i - 3] === input[i - 13]) &&
+      !(input[i - 4] === input[i - 5]) &&
+      !(input[i - 4] === input[i - 6]) &&
+      !(input[i - 4] === input[i - 7]) &&
+      !(input[i - 4] === input[i - 8]) &&
+      !(input[i - 4] === input[i - 9]) &&
+      !(input[i - 4] === input[i - 10]) &&
+      !(input[i - 4] === input[i - 11]) &&
+      !(input[i - 4] === input[i - 12]) &&
+      !(input[i - 4] === input[i - 13]) &&
+      !(input[i - 5] === input[i - 6]) &&
+      !(input[i - 5] === input[i - 7]) &&
+      !(input[i - 5] === input[i - 8]) &&
+      !(input[i - 5] === input[i - 9]) &&
+      !(input[i - 5] === input[i - 10]) &&
+      !(input[i - 5] === input[i - 11]) &&
+      !(input[i - 5] === input[i - 12]) &&
+      !(input[i - 5] === input[i - 13]) &&
+      !(input[i - 6] === input[i - 7]) &&
+      !(input[i - 6] === input[i - 8]) &&
+      !(input[i - 6] === input[i - 9]) &&
+      !(input[i - 6] === input[i - 10]) &&
+      !(input[i - 6] === input[i - 11]) &&
+      !(input[i - 6] === input[i - 12]) &&
+      !(input[i - 6] === input[i - 13]) &&
+      !(input[i - 7] === input[i - 8]) &&
+      !(input[i - 7] === input[i - 9]) &&
+      !(input[i - 7] === input[i - 10]) &&
+      !(input[i - 7] === input[i - 11]) &&
+      !(input[i - 7] === input[i - 12]) &&
+      !(input[i - 7] === input[i - 13]) &&
+      !(input[i - 8] === input[i - 9]) &&
+      !(input[i - 8] === input[i - 10]) &&
+      !(input[i - 8] === input[i - 11]) &&
+      !(input[i - 8] === input[i - 12]) &&
+      !(input[i - 8] === input[i - 13]) &&
+      !(input[i - 9] === input[i - 10]) &&
+      !(input[i - 9] === input[i - 11]) &&
+      !(input[i - 9] === input[i - 12]) &&
+      !(input[i - 9] === input[i - 13]) &&
+      !(input[i - 10] === input[i - 11]) &&
+      !(input[i - 10] === input[i - 12]) &&
+      !(input[i - 10] === input[i - 13]) &&
+      !(input[i - 11] === input[i - 12]) &&
+      !(input[i - 11] === input[i - 13]) &&
+      !(input[i - 12] === input[i - 13])
+    ) {
+      return console.log(i + 1);
+    }
+  }
+}
+
+checkUnique(inputArray);
